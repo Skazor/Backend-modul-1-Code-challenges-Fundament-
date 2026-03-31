9ab61976a283ef8aa0704189ef3df8df8b2bf6c2
Skazor <skage.kaupang@gmail.com>
2026-03-31 14:48:53 +0200

    Oppgave 1 - Variabler

---
+++ CodeChallenges/Program.cs
@@ -0,0 +1,13 @@
+﻿// Oppgave 1 - Variabler
+
+string navn = "Venn Vennlig";
+int alder = 33;
+double høyde = 1.80;
+char forbokstav = 'V';
+bool erStudent = true;
+
+Console.WriteLine(navn);
+Console.WriteLine(alder);
+Console.WriteLine(høyde);
+Console.WriteLine(forbokstav);
+Console.WriteLine(erStudent);
