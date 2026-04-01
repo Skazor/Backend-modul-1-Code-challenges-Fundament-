a656ec1af2aa21c622982d89711f1f09444581bc
Skazor <skage.kaupang@gmail.com>
2026-04-01 10:15:20 +0200

    Oppgave 2 - Brukerinput kalkulator

--- CodeChallenges/Program.cs
+++ CodeChallenges/Program.cs
@@ -19,3 +19,32 @@ Console.WriteLine(calc.Add(10, 5));
 Console.WriteLine(calc.Subtract(10, 5));
 Console.WriteLine(calc.Multiply(10, 5));
 Console.WriteLine(calc.Divide(10, 5));
+
+// Oppgave 2 - Brukerinput
+Console.WriteLine("Velg operasjon: 1=Add, 2=Subtract, 3=Multiply, 4=Divide");
+string valg = Console.ReadLine();
+
+Console.WriteLine("Skriv inn første tall:");
+double tall1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Skriv inn andre tall:");
+double tall2 = double.Parse(Console.ReadLine());
+
+switch (valg)
+{
+    case "1":
+        Console.WriteLine($"Svar: {calc.Add(tall1, tall2)}");
+        break;
+    case "2":
+        Console.WriteLine($"Svar: {calc.Subtract(tall1, tall2)}");
+        break;
+    case "3":
+        Console.WriteLine($"Svar: {calc.Multiply(tall1, tall2)}");
+        break;
+    case "4":
+        Console.WriteLine($"Svar: {calc.Divide(tall1, tall2)}");
+        break;
+    default:
+        Console.WriteLine("Ugyldig valg!");
+        break;
+}
