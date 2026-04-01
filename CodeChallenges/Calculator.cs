aeebfb85dfcf51740089d20aad1f28eeea2b3506
Skazor <skage.kaupang@gmail.com>
2026-04-01 09:57:05 +0200

    Oppgave 2 - Kalkulator

---
+++ CodeChallenges/Calculator.cs
@@ -0,0 +1,27 @@
+public class Calculator
+{
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine("Kan ikke dele på null!");
+            return 0;
+        }
+        return a / b;
+    }
+}
--- CodeChallenges/Program.cs
+++ CodeChallenges/Program.cs
@@ -11,3 +11,11 @@ Console.WriteLine(alder);
 Console.WriteLine(høyde);
 Console.WriteLine(forbokstav);
 Console.WriteLine(erStudent);
+
+// Oppgave 2 - Kalkulator
+Calculator calc = new Calculator();
+
+Console.WriteLine(calc.Add(10, 5));
+Console.WriteLine(calc.Subtract(10, 5));
+Console.WriteLine(calc.Multiply(10, 5));
+Console.WriteLine(calc.Divide(10, 5));
